8571f4a474cf97557947460288e019f4196586ee
Naeem Khedarun <naeem.khedarun@gmail.com>
2012-07-03 18:39:24 +0100

    Added missing tests back in.
    Tests were removed for easier debugging and I forgot to add them back
    in.

--- psclass.Tests.ps1
+++ psclass.Tests.ps1
@@ -3,6 +3,83 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 . "$here\psclass.ps1"
 
+Describe "GivenAnObjectWithMethods_WhenDeserializing" {
+   $testClass = New-PSClass TestObject {
+        note -private myVariable 10
+        method getVariable {
+            return $private.myVariable
+        }
+    }
+    $toSerialize = $testClass.New();
+    
+    Export-Clixml -InputObject $toSerialize -Path .\object.xml
+    $deserialized = Deserialize-PSClass (Import-Clixml .\object.xml)
+
+        It "ItShouldStillHaveMethods" {
+        $deserialized.getVariable().should.be(10)
+    }
+}
+
+Describe "GivenAnObjectWithPublicNotes_AndANonDefaultValue_WhenDeserializing" {
+   $testClass = New-PSClass TestObject {
+        note myVariable 10
+    }
+    $toSerialize = $testClass.New();
+    $toSerialize.myVariable = 8;
+
+    Export-Clixml -InputObject $toSerialize -Path .\object.xml
+    $deserialized = Deserialize-PSClass (Import-Clixml .\object.xml)
+
+    It "ItShouldStillHaveMethods" {
+        $deserialized.myVariable.should.be(8)
+    }
+}
+
+Describe "GivenAnObjectWithStaticNotes_AndANonDefaultValue_WhenDeserializing" {
+   $testClass = New-PSClass TestObject {
+        note -static myVariable 10
+    }
+    $toSerialize = $testClass.New();
+    $toSerialize.Class.myVariable = 8;
+
+    Export-Clixml -InputObject $toSerialize -Path .\object.xml
+    $deserialized = Deserialize-PSClass (Import-Clixml .\object.xml)
+
+    It "ItShouldStillHaveMethods" {
+        $deserialized.Class.myVariable.should.be(8)
+    }
+}
+
+Describe "GivenAnObjectWithAConstructor_WhenDeserializing" {
+   $testClass = New-PSClass TestObject {
+        note executedTimes 0
+        constructor {
+            $this.executedTimes++;
+        }
+    }
+    $toSerialize = $testClass.New();
+
+    Export-Clixml -InputObject $toSerialize -Path .\object.xml
+    $deserialized = Deserialize-PSClass (Import-Clixml .\object.xml)
+
+    It "ItShouldDeserializeWithoutExecuting" {
+        $deserialized.executedTimes.should.be(1)
+    }
+}
+
+Describe "GivenAnObjectWithADifferentNameToTestObject_AndPrivateNotes_WhenDeserializing" {
+   $testClass = New-PSClass AnotherTestObject {
+        note -private executedTimes 0
+    }
+    $toSerialize = $testClass.New();
+
+    Export-Clixml -InputObject $toSerialize -Path .\object.xml
+
+    It "ItShouldDeserializeWithoutErroring" {
+        $deserialized = Deserialize-PSClass (Import-Clixml .\object.xml)
+    }
+}
+
 Describe "GivenAnObjectWithAPropertyWhichIsAPSObject_AndTheBackingFieldIsUpdatedAfterDeserializing_WhenDeserializing" {
     $referencedObject = New-PSClass ReferencedObject {
         note -private myVariable 0
